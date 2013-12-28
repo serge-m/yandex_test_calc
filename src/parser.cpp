694f4b29bc6ed7826628e3937c617b993ee92b4a
Sergey Matyunin <matyunin.vg@gmail.com>
2013-12-29 01:22:14 +0400

    Implementations were moved to source file

--- src/parser.cpp
+++ src/parser.cpp
@@ -90,3 +90,162 @@ double Parser::GetNumberFromString( StackElement se )
     }
     return value;
 }
+
+Parser::Parser()
+{
+
+}
+
+Parser::~Parser()
+{
+
+}
+
+double Parser::Parse( const std::string & s )
+{
+    Stack st;
+    string sc = s + eol;
+    pos_ = 0;
+    res_.clear();
+    operations_.clear();
+    GetExpression2( s );
+    double res = CalcExpression( res_ );
+    return res;
+}
+
+void Parser::GetExpression2( string s )
+{
+    StackElement elem;
+    do 
+    {
+        elem = GetPiece( s );
+
+
+        if( IsPositiveInteger( elem ) )
+        {
+            res_.push_back( elem );
+        }
+        else if( IsOperation( elem ) )
+        {
+            while( !operations_.empty() )
+            {
+                string lastOp = operations_.back();
+                if( lastOp == "(" )
+                    break;
+
+                int prior = Priority( lastOp );
+
+                if( prior < Priority( elem ))
+                    break;
+
+                res_.push_back( lastOp );
+                operations_.pop_back();
+            }
+            if( elem == ")" )
+            {
+
+                string lastOp;
+                if( operations_.empty() || ( lastOp = operations_.back()) != "(" )
+                {
+                    stringstream ss;
+                    ss << "Unexpected ')' in position " << pos_;
+                    throw std::exception( ss.str().c_str() );
+                }
+                operations_.pop_back();
+            }
+            else
+            {
+                operations_.push_back( elem );
+            }
+
+
+        }
+
+
+    } while ( elem != eol );
+}
+
+bool Parser::IsPositiveInteger( const std::string& s )
+{
+    return !s.empty() && 
+        (std::count_if(s.begin(), s.end(), std::isdigit) == s.size());
+}
+
+int Parser::Priority( string s )
+{
+    if( s == "+" || s == "-" )
+    {
+        return 1;
+
+    }
+    else if( s == "*" || s == "/" )
+    {
+        return 2;
+    }
+    else if( s == "(" )
+    {
+        return 4;
+    }
+    else if ( s == ")" )
+    {
+        return 1;
+    }
+    else if( s == eol )
+    {
+        return -1;
+    }
+    else if (IsPositiveInteger( s ))
+    {
+        return 1;
+    }
+    else
+        return 0;
+}
+
+bool Parser::IsOperation( string s )
+{
+    return s == "+" || s == "-" || s == "*" || s == "/" || s == eol || s == "(" || s == ")";
+}
+
+std::string Parser::GetPiece( const std::string & s )
+{
+    string res = "";
+
+    while( pos_ < s.length() && s[pos_] == ' ' ) 
+        ++ pos_;
+
+    if( pos_ >= s.length() )
+    {
+        return eol;
+    }
+
+    int posFirst= pos_;
+
+    string s1 = s.substr( posFirst, 1 );
+    if( IsOperation( s1 ) )
+    {
+        pos_ ++;
+        return s1;
+    }
+
+    while ( pos_ < s.length() && GetSymbolClass( s[pos_]) == GetSymbolClass( s[posFirst ]) )
+    {
+        pos_ ++;    
+    }
+
+    return s.substr( posFirst, pos_ - posFirst );
+}
+
+Parser::SymbolClass Parser::GetSymbolClass( char symbol )
+{
+    if( isalpha( symbol ) )
+        return CLASS_ALPHA;
+    if( isdigit( symbol ) )
+        return CLASS_NUM;
+    if( symbol == '+' || symbol == '-' || symbol == '/' || symbol == '*' )
+        return CLASS_OPERATION;
+    if( isspace( symbol ) )
+        return CLASS_SPACE;
+
+    return CLASS_UNKNOWN;
+}
